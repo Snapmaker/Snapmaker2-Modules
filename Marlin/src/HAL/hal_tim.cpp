77e1b22ccb26583cc75e330dd1f8332c27cbdf3b
xhycode <731374786@qq.com>
2021-03-17 20:30:02 +0800

    Refactor: tim code

--- Marlin/src/HAL/hal_tim.cpp
+++ Marlin/src/HAL/hal_tim.cpp
@@ -22,16 +22,16 @@
 #include "hal_tim.h"
 #include <src/HAL/std_library/inc/stm32f10x_tim.h>
 #include <src/HAL/std_library/inc/stm32f10x.h>
-//                           2     3      4
-TIM_CB_F tim_cb_table[3] = {NULL, NULL, NULL};
+//                           1     2     3     4
+TIM_CB_F tim_cb_table[4] = {NULL, NULL, NULL, NULL};
 static TIM_TypeDef * tim_table[] = {
-  TIM2, TIM3, TIM4
+  TIM1, TIM2, TIM3, TIM4
 };
 uint8_t tim_nvic_iqr_channel[] = {
-  TIM2_IRQn, TIM3_IRQn, TIM4_IRQn
+  TIM1_UP_IRQn, TIM2_IRQn, TIM3_IRQn, TIM4_IRQn
 };
-uint32_t rcc_APB1Periph_tim[] = {
-  RCC_APB1Periph_TIM2, RCC_APB1Periph_TIM3, RCC_APB1Periph_TIM4
+uint32_t rcc_APB_Periph_tim[] = {
+  RCC_APB2Periph_TIM1, RCC_APB1Periph_TIM2, RCC_APB1Periph_TIM3, RCC_APB1Periph_TIM4
 };
 
 static uint8_t tim_check(uint8_t tim) {
@@ -47,11 +47,15 @@ uint16_t        u16Period;       // 装入活动的自动重装载寄存器周�
 */
 void HAL_timer_init(uint8_t tim, uint16_t u16Prescaler, uint16_t u16Period) {
   TIM_TimeBaseInitTypeDef tim_TimeBaseInitTypeDef;
-  tim -= 2;
+  tim -= 1;
   if (!tim_check(tim)) {
     return ;
   }
-  RCC_APB1PeriphClockCmd(rcc_APB1Periph_tim[tim], ENABLE);
+  if (tim == 0) {
+    RCC_APB2PeriphClockCmd(rcc_APB_Periph_tim[tim], ENABLE);
+  } else {
+    RCC_APB1PeriphClockCmd(rcc_APB_Periph_tim[tim], ENABLE);
+  }
   tim_TimeBaseInitTypeDef.TIM_ClockDivision = TIM_CKD_DIV1;
   tim_TimeBaseInitTypeDef.TIM_CounterMode = TIM_CounterMode_Up;
   tim_TimeBaseInitTypeDef.TIM_Period = u16Period - 1;
@@ -68,7 +72,7 @@ uint8_t  SubPriority;          // 响应优先级(0,1,2,3) 数值越小优先级
 */
 void HAL_timer_nvic_init(uint8_t tim, uint8_t PreemptionPriority, uint8_t SubPriority) {
   NVIC_InitTypeDef NVIC_InitStructure;
-  tim -= 2;
+  tim -= 1;
   if (!tim_check(tim)) {
     return ;
   }
@@ -82,7 +86,7 @@ void HAL_timer_nvic_init(uint8_t tim, uint8_t PreemptionPriority, uint8_t SubPri
   TIM_ITConfig(tim_table[tim], TIM_IT_Update, ENABLE);
 }
 void HAL_timer_cb_init(uint8_t tim, TIM_CB_F cb) {
-  tim -= 2;
+  tim -= 1;
   if (!tim_check(tim)) {
     return ;
   }
@@ -90,32 +94,39 @@ void HAL_timer_cb_init(uint8_t tim, TIM_CB_F cb) {
 }
 
 void HAL_timer_enable(uint8_t tim) {
-  TIM_Cmd(tim_table[tim - 2], ENABLE);
+  TIM_Cmd(tim_table[tim - 1], ENABLE);
 }
 #ifdef __cplusplus
 extern "C" {  // only need to export C interface if
               // used by C++ source code
 #endif
 
-void __irq_tim2() {
+void __irq_tim1_up() {
   if (tim_cb_table[0]) {
     tim_cb_table[0]();
   }
-  TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+  TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+}
+
+void __irq_tim2() {
+  if (tim_cb_table[1]) {
+    tim_cb_table[1]();
+  }
+  TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 }
 
 void __irq_tim3() {
-  if (tim_cb_table[1]) {
-    tim_cb_table[1]();
+  if (tim_cb_table[2]) {
+    tim_cb_table[2]();
   }
   TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 }
 
 void __irq_tim4() {
-  if (tim_cb_table[2]) {
-    tim_cb_table[2]();
+  if (tim_cb_table[3]) {
+    tim_cb_table[3]();
   }
-  TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+  TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
 }
 
 #ifdef __cplusplus
